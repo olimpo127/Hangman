d5f1474e84bd1c5dfddb1bbd05250bd01da29d88
F.Ojeda.I <fojedai47@gmail.com>
2023-06-07 20:50:16 +0000

    r

--- src/App.js
+++ src/App.js
@@ -137,7 +137,7 @@ const HangmanGame = () => {
         alignItems: 'center',
         justifyContent: 'center',
         minHeight: '100vh',
-        background: "yellow"
+        background: "red"
       }}
     >
       <h1 style={{ color: 'darkblue', textAlign: 'center' }}>Hangman Game</h1>
